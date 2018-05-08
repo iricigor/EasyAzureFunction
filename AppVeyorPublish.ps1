105de94dd701a7d22b1f91fcc3d13d04f90537a5
Igor Iric <iricigor@gmail.com>
2018-05-08 14:12:39 +0200

    added publish script

---
+++ AppVeyorPublish.ps1
@@ -0,0 +1,22 @@
+# only publish if commit message starts with !publish
+if ($APPVEYOR_REPO_COMMIT_MESSAGE -notmatch '^!publish') {
+    Write-Output "No publishing from AppVeyor, to automatically publish, start commit message with !publish"
+    exit
+}
+
+# check if this version already exists
+$ModuleName = 'EasyAzureFunction'
+
+$Manifest = Test-ModuleManifest -Path (Join-Path . "$ModuleName.psd1")
+$LocalVersion = $Manifest.Version.ToString()
+
+$RemoteModule = Find-Module $ModuleName -Repository PSGallery
+$RemoteVersion = $RemoteModule.Version.ToString()
+
+if ($LocalVersion -eq $RemoteVersion) {
+    throw "Module $ModuleName with version $LocalVersion already exists. Consider bumping version."
+}
+
+# we proceed with publish
+Write-Output "Publishing version $LocalVersion to PSGallery, currently published version is $RemoteVersion"
+Publish-Module -Path . -Repository PSGallery -NuGetApiKey $env:MyPSGalleryAPIKey
